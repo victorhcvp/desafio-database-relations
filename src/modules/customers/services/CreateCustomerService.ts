497092e84136d76c4bfbb486bc6f34291937a915
Victor Hugo <victorhugovergueiro@gmail.com>
2020-12-12 16:56:17 -0400

    envio desafio

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,28 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const findCustomerWithSameEmail = await this.customersRepository.findByEmail(
+      email,
+    );
+
+    if (findCustomerWithSameEmail) {
+      throw new AppError(
+        'There is already an customer with the provided e-mail',
+      );
+    }
+
+    const customer = await this.customersRepository.create({
+      email,
+      name,
+    });
+
+    return customer;
   }
 }
 
